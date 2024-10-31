00b726b22c9bcfd87390482134a7f04b9effaff8
tairhiq <tairhiqharris@yahoo.com>
2024-10-31 09:12:47 -0500

    flower petals

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 // JS scripts placed here
+const hrt = document.querySelector('.heart-Circle');
+
+const toggle = function () {
+	if ( hrt.getAttribute('aria-checked') === 'true' ) {
+		hrt.setAttribute('aria-checked', 'false');
+	}
+	else {
+		hrt.setAttribute('aria-checked', 'true');
+	}
+}
+
+hrt.addEventListener('click', toggle, false);
+hrt.addEventListener('keypress', function ( e ) {
+	const keyCode = e.keyCode || e.which;
+	switch ( keyCode ) {
+		case 32:
+			e.preventDefault();
+			toggle();
+			break;
+		default:
+			break;
+	}
+	
+})
